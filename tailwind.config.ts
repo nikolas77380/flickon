7dfca8bb3f38d9764506944675eb78c0f35b0692
nik <mykola.kipniak@fishbrain.com>
2024-03-08 12:04:51 +0200

    test git restrict

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
+   content: [
     "./node_modules/flowbite-react/lib/**/*.js",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
